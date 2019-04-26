35758949a36d97f08a7bc05695d5254f0935dab4
187107363 <43198174+187107363@users.noreply.github.com>
2019-04-26 12:33:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
-var serveStatic = require("serve-static")
 var path = require('path');
-const history = require('connect-history-api-fallback');
+var serveStatic = require('serve-static');
 var app = express();
-app.use(history({
-    disableDotRule: true,
-    verbose: true
-}));
-app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/dist/index.html'));
-});
-app.use(serveStatic(path.join(__dirname, 'dist')));
-var port = process.env.PORT || 80;
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
