06e18e2e9421c242840d6cb65d87042334e94194
187107363 <43198174+187107363@users.noreply.github.com>
2019-04-26 12:39:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// server.js
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
+//added this middleware
+var history = require('connect-history-api-fallback');
+
+var app = express()
+//started middleware
+app.use(history());
+
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
